352dae9ae906ea07c0bcdc175ea9aace66b57ce8
danielderma <danielderma.contact@gmail.com>
2023-02-07 01:39:18 -0600

    error

--- src/pages/api/example.ts
+++ src/pages/api/example.ts
@@ -1,11 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { example } from "@/services/cohere";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const response = await example();
-  res.status(200).json(response);
+  res.status(200).json({
+    name: "ok",
+  });
 }
